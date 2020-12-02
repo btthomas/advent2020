22f4e008d41ae250812071e882423cca3a1212e5
Blake Thomas <blake.thomas@id.me>
2020-12-02 17:29:20 -0500

    one-b

--- one.rb
+++ one.rb
@@ -2,14 +2,24 @@ values = []
 
 text = File.readlines("one.txt").each do |line|
   value = line.to_i
+  values << value
+end
 
-  values.each do |old_value|
-    if (old_value + value == 2020)
-      puts (old_value * value)
+values = values.sort
+
+values.each_with_index do |first, i1|
+  
+  values.each_with_index do |second, i2|
+    next if (i2 <= i1)
+    
+    values.each_with_index do |third, i3|
+      next if (i3 <= i2)
+
+      if (first + second + third == 2020)
+        puts (first * second * third)
+      end
     end
   end
-
-  values << value
 end
 
 # puts values.join(', ')
