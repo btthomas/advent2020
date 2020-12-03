ec1d2792d1752058f5cdb3337593ec1c630166a0
Blake Thomas <blake.thomas@id.me>
2020-12-03 16:22:18 -0500

    three-b

--- three.rb
+++ three.rb
@@ -1,11 +1,4 @@
-def main
-  i = 0
-  grid = []
-  File.readlines("three.txt").each do |line|
-    grid[i] = line.chomp
-    i += 1
-  end
-
+def calc(grid, x_speed, y_speed)
   width = grid[0].length
 
   trees = 0
@@ -14,11 +7,40 @@ def main
 
   while y < grid.length
     trees += 1 if grid[y][x % width] == '#'
-    y += 1
-    x += 3
+    y += y_speed
+    x += x_speed
   end
 
-  puts trees
+  trees
+end
+
+def main
+  i = 0
+  grid = []
+  File.readlines("three.txt").each do |line|
+    grid[i] = line.chomp
+    i += 1
+  end
+
+  slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ]
+
+  counts = []
+  product = 1
+
+  slopes.each do |slope|
+    count = calc(grid, *slope)
+    counts << count
+    product *= count
+  end
+
+  puts counts
+  puts product
 end
 
 main
