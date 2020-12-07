387c3a96a6168543612466812689c76cb925a283
Blake Thomas <blake.thomas@id.me>
2020-12-07 13:28:25 -0500

    seven-b

--- seven.rb
+++ seven.rb
@@ -1,4 +1,5 @@
 DONE = []
+SUM = {}
 
 def parse_rule(string)
   in_to_out = {}
@@ -53,14 +54,62 @@ def main
     
   end
 
-  # puts rules
-
   calc(rules, "shiny gold")
 
-  # puts '--', DONE, '--'
-
   puts DONE.uniq.length
-
 end
 
-main
+def parse_rule_b(string)
+  out_to_in = {}
+
+  outer_string, inner_string = string.split('contain')
+  outer_type = outer_string[0..-7]
+
+  return Hash[outer_type, []] if inner_string == " no other bags."
+
+  array = []
+  inners = inner_string.split(',')
+  inners.each do |inner|
+    num, mod, color, rest = inner.strip.split(' ')
+
+    array << { num: num.to_i, type: [mod, color].join(' ')}
+  end
+
+  out_to_in[outer_type] = array
+
+  out_to_in
+end
+
+def calcb(rules, type)
+  return SUM[type] if SUM[type]
+
+  contents = rules[type]
+  
+  return 0 if contents.length == 0
+  sum = 0
+
+  contents.each do |rule|
+    add = rule[:num] + rule[:num] * calcb(rules, rule[:type])
+    sum += add
+  end
+
+  SUM[type] = sum
+  sum
+end
+
+def mainb
+  rules = {}
+  File.readlines("seven.txt").each do |line|
+    string = line.chomp
+
+    rule = parse_rule_b(string)
+
+    rules = rules.merge(rule)
+  end
+
+  total = calcb(rules, "shiny gold")
+
+  puts total
+end
+
+mainb
